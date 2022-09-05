041d421fffd23d8c912c333195a8e411b7165caa
Ole Hüter <57348167+ohueter@users.noreply.github.com>
2022-09-05 16:45:52 +0200

    add bottles

--- Formula/autokbisw.rb
+++ Formula/autokbisw.rb
@@ -1,12 +1,19 @@
 class Autokbisw < Formula
   desc "Automatic keyboard/input source switching for macOS"
   homepage "https://github.com/ohueter/autokbisw"
+  license "Apache-2.0"
+  depends_on :xcode
+
   version "1.4.1"
   url "https://github.com/ohueter/autokbisw/archive/refs/tags/1.4.1.tar.gz"
   sha256 "1666456372e53f87d29ad2b5402480c0e62839737dabeb93c38387fae50598e4"
 
-  depends_on :xcode
-    
+  bottle do
+    root_url "https://github.com/ohueter/autokbisw/releases/download/v1.4.1"
+    sha256 monterey: "a8596733b42fd2c882ecfd677ec225844665b9856fc2eb35bb1bffcb21d6bf15"
+    sha256 big_sur:  "28359ae565be040666913bc491fcfb81d7453880914ea664fec61762f298fe6c"
+  end
+
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox", "--arch", "arm64", "--arch", "x86_64"
     bin.install ".build/apple/Products/Release/autokbisw"
