7012579b9ad5d27dae75e9b1637101b23afb1f12
Ole Hüter <57348167+ohueter@users.noreply.github.com>
2022-09-07 10:16:15 +0200

    revert remove disable sandbox

--- Formula/autokbisw.rb
+++ Formula/autokbisw.rb
@@ -8,7 +8,7 @@ class Autokbisw < Formula
   depends_on :xcode
     
   def install
-    system "swift", "build", "-c", "release"
+    system "swift", "build", "-c", "release", "--disable-sandbox"
     bin.install ".build/release/autokbisw"
   end
     
