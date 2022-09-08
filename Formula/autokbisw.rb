d2e7dc88b8b717d1a16c093078060e5fce0aeb73
Ole Hüter <57348167+ohueter@users.noreply.github.com>
2022-09-08 15:07:42 +0000

    autokbisw: add 1.4.3 bottle.

--- Formula/autokbisw.rb
+++ Formula/autokbisw.rb
@@ -3,6 +3,13 @@ class Autokbisw < Formula
   homepage "https://github.com/ohueter/autokbisw"
   url "https://github.com/ohueter/autokbisw/archive/refs/tags/1.4.3.tar.gz"
   sha256 "c5af10bbc125b53e58731fd00bcb6a8a2909a5aab568c97e1d3e54952e72257f"
+
+  bottle do
+    root_url "https://github.com/ohueter/homebrew-tap/releases/download/autokbisw-1.4.3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey: "c1ca98af9b6cc17acd213a780faa6c6e4591b5ea705c1d7c17d349b765c0bc5a"
+    sha256 cellar: :any_skip_relocation, big_sur:  "62cacf1207687d252b9ff02f6ae81293b21368c329ecb933a88555c8633fac72"
+  end
   depends_on :xcode
 
   def install
