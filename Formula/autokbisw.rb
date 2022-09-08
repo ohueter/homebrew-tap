beaf1d6c63de4694480aff8fd51febc5cf0b81f9
Ole Hüter <57348167+ohueter@users.noreply.github.com>
2022-09-08 14:48:52 +0200

    fix indentation

--- Formula/autokbisw.rb
+++ Formula/autokbisw.rb
@@ -15,26 +15,26 @@ class Autokbisw < Formula
 
   def plist
     <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{bin}/autokbisw</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/autokbisw.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/autokbisw.log</string>
-      </dict>
-    </plist>
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{bin}/autokbisw</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/autokbisw.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/autokbisw.log</string>
+        </dict>
+      </plist>
     EOS
   end
 
