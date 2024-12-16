bddb4532a805975e2478e89572e0a4165caa1a1f
Ole Hüter <57348167+ohueter@users.noreply.github.com>
2024-12-16 12:56:41 +0100

    release autokbisw 2.0.1

--- Formula/autokbisw.rb
+++ Formula/autokbisw.rb
@@ -1,9 +1,9 @@
 class Autokbisw < Formula
   desc "Automatic keyboard input language switching for macOS"
   homepage "https://github.com/ohueter/autokbisw"
-  version "2.0.0"
-  url "https://github.com/ohueter/autokbisw/archive/refs/tags/2.0.0.tar.gz"
-  sha256 "25b04edf093b2b19c2c58c3496b1125fb338fce944e31a8c25c7df7aa48da44a"
+  version "2.0.1"
+  url "https://github.com/ohueter/autokbisw/archive/refs/tags/2.0.1.tar.gz"
+  sha256 "f68c2dd6f1b05058ec57202023a5a628b9ff7dba0fa86e58e2a4219a1c26ef2d"
   depends_on :xcode
 
   def install
